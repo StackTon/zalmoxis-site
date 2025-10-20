dbd15f3473a468683e4d04e6c689b7974215702b
a4i <stanoev1337@gmail.com>
2025-10-20 18:18:11 +0300

    add links to header menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,18 @@
+import Link from 'next/link';
 import { ColorThemeSwitch } from "@/components/Header/ColorThemeSwitch";
 
 export function Header() {
 
   return <header className="flex mt-8 mb-6 ml-6 mr-6">
     <div className="flex-1">
-      Zalmoxis Labs
+      <Link href="/">Zalmoxis Labs</Link>
     </div>
     <ul className="flex flex-10 justify-center text-[var(--color-gray-1300)]">
-      <li className="pr-5">Work</li>
-      <li className="pr-5">Services</li>
-      <li className="pr-5">About</li>
-      <li className="pr-5">Resume</li>
-      <li>Contact</li>
+      <li className="pr-5"><Link href="/work">Work</Link></li>
+      <li className="pr-5"><Link href="/services">Services</Link></li>
+      <li className="pr-5"><Link href="/about">About</Link></li>
+      <li className="pr-5"><Link href="/resume">Resume</Link></li>
+      <li><Link href="/contact">Contact</Link></li>
     </ul>
     <ColorThemeSwitch />
   </header>

224a4812c77d64010bd341e14ae3d2241fb57159
a4i <stanoev1337@gmail.com>
2025-10-21 15:11:36 +0300

    chore: add hover style to header links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,17 +2,18 @@ import Link from 'next/link';
 import { ColorThemeSwitch } from "@/components/Header/ColorThemeSwitch";
 
 export function Header() {
+  const linkClassname = "pr-5 hover:text-[var(--foreground)] hover:underline"
 
   return <header className="flex mt-8 mb-6 ml-6 mr-6">
-    <div className="flex-1">
+    <div className="flex-1 underline">
       <Link href="/">Zalmoxis Labs</Link>
     </div>
     <ul className="flex flex-10 justify-center text-[var(--color-gray-1300)]">
-      <li className="pr-5"><Link href="/work">Work</Link></li>
-      <li className="pr-5"><Link href="/services">Services</Link></li>
-      <li className="pr-5"><Link href="/about">About</Link></li>
-      <li className="pr-5"><Link href="/resume">Resume</Link></li>
-      <li><Link href="/contact">Contact</Link></li>
+      <li className={linkClassname}><Link href="/work">Work</Link></li>
+      <li className={linkClassname}><Link href="/services">Services</Link></li>
+      <li className={linkClassname}><Link href="/about">About</Link></li>
+      <li className={linkClassname}><Link href="/resume">Resume</Link></li>
+      <li className={linkClassname}><Link href="/contact">Contact</Link></li>
     </ul>
     <ColorThemeSwitch />
   </header>

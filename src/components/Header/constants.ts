aa031f196dcf950c8f8424bbccfb4f11af614e36
a4i <stanoev1337@gmail.com>
2025-10-23 16:05:20 +0300

    chore: create global pages constatns and use it in HEADER_LINKS

---
+++ src/components/Header/constants.ts
@@ -0,0 +1,9 @@
+import { PAGES } from "@/app/constants";
+
+export const HEADER_LINKS = {
+  Work: PAGES.Work,
+  Services: PAGES.Services,
+  About: PAGES.About,
+  Resume: PAGES.Resume,
+  Contact: PAGES.Contact,
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,23 @@
 import Link from 'next/link';
 import { ColorThemeSwitch } from "@/components/Header/ColorThemeSwitch";
-import { Pages } from '@/components/Header/constants';
+import { HEADER_LINKS } from '@/components/Header/constants';
 
-const entries = Object.entries(Pages)
+ const links = Object.entries(HEADER_LINKS)
 
 export function Header() {
   const linkClassname = "pr-5 hover:text-[var(--foreground)] hover:underline"
+
 
   return <header className="flex mt-8 mb-6 ml-6 mr-6">
     <div className="flex-1 underline">
       <Link href="/">Zalmoxis Labs</Link>
     </div>
     <ul className="flex flex-10 justify-center text-[var(--color-gray-1300)]">
-      {entries.map(([label, href]) => (
-        <li className={linkClassname}><Link href={href}>{label}</Link></li>
-      ))}
+      {links.map(([label, href]) => {
+        return <li className={linkClassname}><Link href={href}>{label}</Link></li>
+      }
+        
+      )}
     </ul>
     <ColorThemeSwitch />
   </header>
